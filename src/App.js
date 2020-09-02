b296e95e5157787e289e4023090ee8c5c62cadb7
manik-roy <cm.dpi15@gmail.com>
2020-09-02 17:11:12 +0600

    todo apps done

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TodoItem from './Components/todoItem/TodoItem';
+
+
+function App() {
+
+  const [newTodo, setNewTodo] = useState('')
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const items = localStorage.getItem('todos');
+    items && setTodos(JSON.parse(items))
+  }, [])
+
+  // add new todo
+  const addTodo = () => {
+    const createTodo = {
+      name: newTodo,
+      isActive: true,
+      id: todos.length + 1
+    }
+    const newTodos = [...todos, createTodo];
+    setTodos(newTodos)
+    localStorage.setItem('todos', JSON.stringify(newTodos))
+  }
+
+  // remove todos
+  const removeTodo = id => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    localStorage.setItem('todos', JSON.stringify(newTodos))
+    setTodos(newTodos)
+  }
+
+  // done todo
+  const doneTodo = id => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isActive = false;
+      }
+      return todo;
+    })
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="col-md-4 mx-auto px-2">
+      <div className="todo justify-content-center mt-5">
+        <div class="input-group mb-2 mr-sm-2">
+          <input type="text" name="todo" value={newTodo} 
+            onChange={(e) => setNewTodo(e.target.value)} className="form-control" />
+          <div class="input-group-append">
+            <button className="btn btn-secondary" onClick={addTodo}>Add todo</button>
+          </div>
+        </div>
+      </div>
+      <div className="todo-container mt-4">
+        <ul className="list-group">
+          {todos.map(todo => <TodoItem todo={todo} removeTodo={removeTodo} doneTodo={doneTodo} />)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/todoItem/TodoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+  const { name, isActive, id } = props.todo;
+  return (
+    <div className="list-group-item align-items-center d-flex justify-content-between border-bottom shadow my-1" >
+      <span className={isActive ? '' : 'line-through'} >{name}</span>
+      <div className="buttons-group my-2">
+        <button
+          onClick={() => props.doneTodo(id)}
+          className="btn btn-sm btn-success mr-2"><i class="fa fa-check"></i>
+        </button>
+        <button
+          onClick={() => props.removeTodo(id)}
+          className="btn btn-sm btn-danger"><i class="fa fa-trash"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
